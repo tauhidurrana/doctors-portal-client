143571a8a201e2f006740051bbd16c60e60ef5de
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-11 13:54:39 +0600

    serices added by loop

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import flouride from './../assets/images/fluoride.png';
+import cavity from './../assets/images/cavity.png';
+import whitening from './../assets/images/whitening.png';
+import Service from './Service';
 
 const Services = () => {
+    const services = [
+        {
+            _id: 1,
+            name: "Flouride Treatment",
+            description: "",
+            img: flouride,
+        },
+        {
+            _id: 2,
+            name: "Flouride Treatment",
+            description: "",
+            img: cavity,
+        },
+        {
+            _id: 3,
+            name: "Flouride Treatment",
+            description: "",
+            img: whitening,
+        },
+    ]
     return (
         <div className='my-28'>
             <div className='text-center'>
                 <h3 className='text-primary text-xl font-bold uppercase'>Our Services</h3>
                 <h2 className=' text-4xl'>Service We Provide</h2>
             </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-12 gap-6'>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
+
         </div>
     );
 };
