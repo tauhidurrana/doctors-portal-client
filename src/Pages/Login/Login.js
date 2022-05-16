f2629cfa27387003a63b337bac4fa369ffbf30ca
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-16 20:53:12 +0600

    signIn with email & password added and loading spinne added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import Loading from '../Shared/Loading';
 
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
 
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
 
-    if (user) {
-        console.log(user);
+      let signInError;
+
+    if (gUser) {
+        console.log(gUser);
     }
 
+    if(error || gError){
+        signInError=<p className='text-red-500'>{error?.message || gError?.message}</p>
+    }
+
+    if( loading || gLoading){
+        return <Loading></Loading>
+    }
     const onSubmit = data => {
         console.log(data);
+        signInWithEmailAndPassword(data.email, data.password)
     }
 
 
@@ -74,6 +92,7 @@ const Login = () => {
                                 {errors.password?.type === 'minLength' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
                             </label>
                         </div>
+                        {signInError}
                             <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
                     </form>
 
