7e0405f457fa411fdce39397025d24e4d3a30eb9
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-16 11:47:15 +0600

    firebase config, initialization and env config added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -8,7 +8,7 @@ const AvailableAppointments = ({date}) => {
     const [treatment, setTreatment] = useState(null);
     
     useEffect(()=>{
-        fetch('services.json')
+        fetch('http://localhost:5000/service')
         .then(res=>res.json())
         .then(data=>setServices(data))
     },[])
@@ -25,7 +25,11 @@ const AvailableAppointments = ({date}) => {
                 }
             </div>
             {/* treatment pass to booking model if condition true */}
-            {treatment && <BookingModal treatment={treatment}></BookingModal>}
+            {treatment && <BookingModal 
+            date={date}
+            treatment={treatment}
+            setTreatment ={setTreatment}
+            ></BookingModal>}
         </div>
     );
 };
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,17 +1,39 @@
+import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({treatment}) => {
+const BookingModal = ({ date, treatment, setTreatment }) => {
+    const { name, slots } = treatment;
+
+    const handleBooking = event =>{
+        event.preventDefault();
+        const slot = event.target.slot.value;
+        console.log(slot);
+        setTreatment(null);
+    }
     return (
         <div>
             <input type="checkbox" id="booking-modal" class="modal-toggle" />
             <div class="modal modal-bottom sm:modal-middle">
                 <div class="modal-box">
-                <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
-                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                    <div class="modal-action">
+                    <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 class="font-bold text-lg text-secondary">Booking For: {name}!</h3>
+
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-4 justify-items-center mt-3'>
+                        <input type="text" readOnly disabled value={format(date, 'PP')} class="input input-bordered w-full max-w-xs" />
+                        <select name='slot' class="select select-bordered w-full max-w-xs">
+                            {
+                                slots.map(slot=><option value={slot}>{slot}</option>)
+                            }
+                        </select>
+                        <input type="text" name='name' placeholder="Full Name" class="input input-bordered w-full max-w-xs" />
+                        <input type="text" name='phone' placeholder="Phone Number" class="input input-bordered w-full max-w-xs" />
+                        <input type="email" name='email' placeholder="Email" class="input input-bordered w-full max-w-xs" />
+                        <input type="submit" value="SUBMIT" placeholder="Type here" class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary w-full max-w-xs" />
+                    </form>
+
+                    {/* <div class="modal-action">
                         <label for="booking-modal" class="btn">Yay!</label>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
