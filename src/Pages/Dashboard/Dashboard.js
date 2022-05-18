ec1cca9c5438143dcab0f061b665256e3648d8bc
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-18 17:13:42 +0600

    dashboard added with dynamic data

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -5,8 +5,8 @@ const Dashboard = () => {
     return (
         <div class="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content flex flex-col items-center justify-center">
-                <h2 className='text-5xl text-purple-500'>Dashboard</h2>
+            <div class="drawer-content">
+                <h2 className='text-3xl text-purple-500 font-bold'>Dashboard</h2>
                 <Outlet></Outlet>
                 <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
